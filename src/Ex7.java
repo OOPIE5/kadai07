1fe8ac40f07c91c3c4afe4bd5f5296ee9ffdcbae
重村(SHIGEMURA)　哲至(TETSUJI) <sigemura@tokuyama.kosen-ac.jp>
2022-10-13 11:30:46 +0900

    TABをスペースに置換え
    	modified:   src/CurrentColor.java
    	modified:   src/Ex7.java

--- src/Ex7.java
+++ src/Ex7.java
@@ -10,74 +10,74 @@ import java.awt.event.ActionEvent;
 
 public class Ex7 {
 
-	private JFrame frame;
-	private JPanel colorPanel;
-	private CurrentColor crrntBckColor = new CurrentColor();
+  private JFrame frame;
+  private JPanel colorPanel;
+  private CurrentColor crrntBckColor = new CurrentColor();
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Ex7 window = new Ex7();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+  /**
+   * Launch the application.
+   */
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          Ex7 window = new Ex7();
+          window.frame.setVisible(true);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
 
-	/**
-	 * Create the application.
-	 */
-	public Ex7() {
-		initialize();
-	}
+  /**
+   * Create the application.
+   */
+  public Ex7() {
+    initialize();
+  }
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		JPanel btnPanel = new JPanel();
-		frame.getContentPane().add(btnPanel, BorderLayout.NORTH);
-		
-		JButton btnRed = new JButton("Red");
-		btnRed.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Color c = crrntBckColor.changeRed();
-				colorPanel.setBackground(c);
-			}
-		});
-		btnPanel.add(btnRed);
-		
-		JButton btnGreen = new JButton("Green");
-		btnGreen.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Color c = crrntBckColor.changeGreen();
-				colorPanel.setBackground(c);
-			}
-		});
-		btnPanel.add(btnGreen);
-		
-		JButton btnBlue = new JButton("Blue");
-		btnBlue.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Color c = crrntBckColor.changeBlue();
-				colorPanel.setBackground(c);
-			}
-		});
-		btnPanel.add(btnBlue);
-		
-		colorPanel = new JPanel();
-		colorPanel.setBackground(Color.BLACK);
-		frame.getContentPane().add(colorPanel, BorderLayout.CENTER);
-	}
+  /**
+   * Initialize the contents of the frame.
+   */
+  private void initialize() {
+    frame = new JFrame();
+    frame.setBounds(100, 100, 450, 300);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    JPanel btnPanel = new JPanel();
+    frame.getContentPane().add(btnPanel, BorderLayout.NORTH);
+    
+    JButton btnRed = new JButton("Red");
+    btnRed.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        Color c = crrntBckColor.changeRed();
+        colorPanel.setBackground(c);
+      }
+    });
+    btnPanel.add(btnRed);
+    
+    JButton btnGreen = new JButton("Green");
+    btnGreen.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        Color c = crrntBckColor.changeGreen();
+        colorPanel.setBackground(c);
+      }
+    });
+    btnPanel.add(btnGreen);
+    
+    JButton btnBlue = new JButton("Blue");
+    btnBlue.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        Color c = crrntBckColor.changeBlue();
+        colorPanel.setBackground(c);
+      }
+    });
+    btnPanel.add(btnBlue);
+    
+    colorPanel = new JPanel();
+    colorPanel.setBackground(Color.BLACK);
+    frame.getContentPane().add(colorPanel, BorderLayout.CENTER);
+  }
 
 }
